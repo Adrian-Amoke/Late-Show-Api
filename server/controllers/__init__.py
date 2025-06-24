0ca04cc4db64698228cba4e8eacb81b3e5ee6ae9
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-06-24 15:19:41 +0300

    added blueprint imports to controllers/__init__.py

--- server/controllers/__init__.py
+++ server/controllers/__init__.py
@@ -1,1 +1,6 @@
 # Controllers package initialization
+
+from .appearance_controller import appearance_bp
+from .auth_controller import auth_bp
+from .episode_controller import episode_bp
+from .guest_controller import guest_bp
